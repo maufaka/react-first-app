287f4afc63b4d8945725bde0bf30650dfca458d3
maufaka <maufaka47@gmail.com>
2020-04-03 22:39:14 +0300

    containerComponent

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,51 +1,31 @@
-import React from "react";
+import React from 'react';
 import classes from './Friends.module.css';
-import * as axios from "axios";
 import userPhoto from "./../../assets/images/user.png"
 
-class Friends extends React.Component {
-  
-  componentDidMount() {
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).
-      then(response => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount)
-      });
-  }
+let Friends = (props) => {
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).
-      then(response => {
-        this.props.setUsers(response.data.items)
-      });
-  }
-
-  render = () => {
-
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
     let pages = [ ];
     for (let i=1; i<=pagesCount; i++) {
       pages.push(i);
     }
 
-    return <div className={classes.friends}>
-         <div className={classes.pagination}>
-          {pages.map( p => {
-              return <span onClick={()=> {this.onPageChanged(p)}} className={this.props.currentPage === p && classes.selectedPage}> {p} </span>
-            })}
-         </div>
-         {this.props.users.map(u => 
-           <div key={u.id}>
-             <img className={classes.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="ava"/>
-             <span>{u.name}</span> <br/>
-              {u.followed 
-             ? <button onClick={() => { this.props.unfollow(u.id) } }>Unfollow</button> 
-             : <button onClick={() => { this.props.follow(u.id) } } >Follow</button>}
-           </div>)}
-       </div>
-    }
-};
+    return (<div className={classes.friends}>
+    <div className={classes.pagination}>
+     {pages.map( p => {
+         return <span onClick={()=> {props.onPageChanged(p)}} className={props.currentPage === p && classes.selectedPage}> {p} </span>
+       })}
+    </div>
+    {props.users.map(u => 
+      <div key={u.id}>
+        <img className={classes.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="ava"/>
+        <span>{u.name}</span> <br/>
+         {u.followed 
+        ? <button onClick={() => { props.unfollow(u.id) } }>Unfollow</button> 
+        : <button onClick={() => { props.follow(u.id) } } >Follow</button>}
+      </div>)}
+  </div>)
+}
 
 export default Friends;
--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,6 +1,37 @@
+import React from 'react';
 import { connect } from "react-redux";
+import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC} from './../../redux/users-reducer';
 import Friends from "./Friends";
-import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC} from './../../redux/users-reducer'
+import * as axios from "axios";
+
+class FriendsAPIComponent extends React.Component {
+  
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).
+        then(response => {
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount)
+        });
+    }
+  
+    onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber);
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).
+        then(response => {
+          this.props.setUsers(response.data.items)
+        });
+    }
+  
+    render = () => {
+      return <Friends totalUsersCount={this.props.totalUsersCount}
+                      currentPage={this.props.currentPage}
+                      pageSize={this.props.pageSize}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}/>
+      }
+  };
 
 const mapStateToProps = (state) => {
     return {
@@ -21,5 +52,5 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Friends);
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsAPIComponent);
 
